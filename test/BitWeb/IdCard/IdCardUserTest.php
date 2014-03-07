b4d4d93350dd60ecef64ce62b07b5380eb630a8e
rain <rain@bitweb.ee>
2014-03-07 17:50:37 +0200

    Tests for IdCardUser

--- test/BitWeb/IdCard/IdCardUserTest.php
+++ test/BitWeb/IdCard/IdCardUserTest.php
@@ -6,16 +6,25 @@ class IdCardUserTest extends \PHPUnit_Framework_TestCase
 {
     public function testCanCreateIdCardUser()
     {
+        new IdCardUser('John', 'Doe', '38601012796', 'EST');
+    }
+
+    public function testSettersAndGetters()
+    {
         $firstName = 'John';
         $lastName = 'Doe';
-        $personalCode = '38601012796';
+        $socialSecurityNumber = '38601012796';
         $country = 'EST';
 
-        $user = new IdCardUser($firstName, $lastName, $personalCode, $country);
+        $user = new IdCardUser();
+        $user->setCountry($country);
+        $user->setFirstName($firstName);
+        $user->setLastName($lastName);
+        $user->setSocialSecurityNumber($socialSecurityNumber);
 
         $this->assertEquals($firstName, $user->getFirstName());
         $this->assertEquals($lastName, $user->getLastName());
-        $this->assertEquals($personalCode, $user->getSocialSecurityNumber());
+        $this->assertEquals($socialSecurityNumber, $user->getSocialSecurityNumber());
         $this->assertEquals($country, $user->getCountry());
     }
 }
